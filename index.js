e65290245ea96ead73931b7df659993e516a0274
Quang Tín <63833652+quangtin131299@users.noreply.github.com>
2020-11-11 15:19:47 +0800

    cap nhat 11/11/2020 15:19

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const express = require("express");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const app = express();
+const groupBy = require("json-groupby");
+const { json } = require("express");
 
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT || 3000);
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
@@ -21,13 +20,7 @@ const conn = mysql.createConnection({
   port: 3306,
 });
 
-
-app.get("/", function(req, res) {
-  conn.query("SELECT * FROM ghe", function(err, result) {
-    res.json(result);
-  });
-});
-
+//#region Phần Client
 app.post("/login", function(req, res) {
   let sqlquery = `SELECT khachhang.ID, khachhang.HoTen, khachhang.Email,DATE_FORMAT(khachhang.NgaySinh, '%d/%m/%Y') as NgaySinh, khachhang.SDT,khachhang.Account, khachhang.Password FROM khachhang WHERE Account = '${req
     .body.account}' AND Password = '${req.body.password}'`;
@@ -101,7 +94,20 @@ app.get("/loadphimdangchieu", function(req, res) {
     if (err) {
       console.log(err);
     } else {
-      res.json(result);
+      let temptenphim = "";
+      let mangkq = [];
+      for (let i = 0; i < result.length; i++) {
+        if (result[i].TenPhim !== temptenphim) {
+          for (let j = i + 1; j < result.length; j++) {
+            if (result[j].TenPhim === result[i].TenPhim) {
+              result[i].TenLoai += ", " + result[j].TenLoai;
+            }
+          }
+          temptenphim = result[i].TenPhim;
+          mangkq.push(result[i]);
+        }
+      }
+      res.json(mangkq);
     }
   });
 });
@@ -150,37 +156,33 @@ app.get("/datvephim", function(req, res) {
   let status = req.query.status;
   let idphong = req.query.idphong;
 
-
   let sqlqueryhoadon = `INSERT INTO hoadon  VALUES (NULL, '${ngaydat}', '45000', '${idkhachhang}', 'Chưa thanh toán');`;
-  
-  conn.query(sqlqueryhoadon, function(err, result){
-    if(err){
+
+  conn.query(sqlqueryhoadon, function(err, result) {
+    if (err) {
       console.log(err);
-    }else{
+    } else {
       let sqllastrowhoadon = `SELECT * FROM hoadon ORDER BY hoadon.ID DESC LIMIT 1`;
-      conn.query(sqllastrowhoadon, function(err, result){
+      conn.query(sqllastrowhoadon, function(err, result) {
         let idhoadon = result[0].ID;
         let sqlquery = `INSERT INTO vedat VALUES (NULL, '${ngaydat}', '${idsuat}', '${idghe}', '${idphim}', '${idkhachhang}', '${idrap}', '${idhoadon}', '${status}', '${idphong}')`;
         conn.query(sqlquery, function(err, result) {
           if (err) {
-           console.log(err);
+            console.log(err);
           } else {
             res.send("Đặt vé thành công!");
           }
         });
-        
+
         let sqlqueryghephong = `INSERT INTO ghe_phong VALUES ('${idghe}', '${idphong}','${idsuat}','${status}' ,'${ngaydat}')`;
-        conn.query(sqlqueryghephong, function(err,result){
-            if(err){
-              console.log(err);
-            }
+        conn.query(sqlqueryghephong, function(err, result) {
+          if (err) {
+            console.log(err);
+          }
         });
-      })
-    
+      });
     }
-  })
-
-
+  });
 });
 
 app.get("/loadxuatchieu", function(req, res) {
@@ -262,6 +264,217 @@ app.post("/capnhatthongtinkhach", function(req, res) {
     }
   });
 });
+//#endregion
 
+//#region Phần Server
 
+//Movie
+app.get("/loadphimadmin", function(req, res) {
+  let soluong = req.query.soluong;
+  let vitri = req.query.vitri;
+
+  let query = `SELECT phim.ID, phim.TenPhim, phim.Hinh, phim.TrangThai, phim.ThoiGian, phim.Trailer, phim_loaiphim.MoTa, DATE_FORMAT(phim_loaiphim.NgayKhoiChieu, '%d/%m/%Y') as 'NgayKhoiChieu' FROM phim JOIN phim_loaiphim ON phim.ID = phim_loaiphim.ID_Phim`;
+  conn.query(query, function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      let temptenphim = "";
+      let mangkq = [];
+      for (let i = 0; i < result.length; i++) {
+        if (result[i].TenPhim !== temptenphim) {
+          for (let j = i + 1; j < result.length; j++) {
+            if (result[j].TenPhim === result[i].TenPhim) {
+              result[i].TenLoai += ", " + result[j].TenLoai;
+            }
+          }
+          temptenphim = result[i].TenPhim;
+          mangkq.push(result[i]);
+        }
+      }
+      res.json(mangkq);
+    }
+  });
+});
+app.get("/timkiemmovieadmin", function(req, res) {
+  let tenphim = req.query.tenphim;
+  let sqlquery = `SELECT * FROM phim WHERE phim.TenPhim LIKE N'%${tenphim}%'`;
+  conn.query(sqlquery, function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.post("/updatemovie", function(req, res) {
+  let maphim = req.body.maphim;
+  let tenphim = req.body.tenphim;
+  let hinhphim = req.body.hinhphim;
+  let trangthai = req.body.trangthai;
+  let thoigian = req.body.thoigian;
+  let idtrailer = req.body.idtrailer;
+  let mota = req.body.mota;
+
+  let sqlquery = `UPDATE phim
+  SET phim.TenPhim = N'${tenphim}', phim.Hinh = '${hinhphim}', phim.TrangThai = N'${trangthai}', phim.ThoiGian = ${thoigian}, phim.Trailer = '${idtrailer}'
+  WHERE phim.ID = ${maphim}`;
+
+  conn.query(sqlquery, function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send("Cập nhật thành công !");
+    }
+  });
+});
+
+app.post("/insertnewmovieadmin", function(req, res) {
+  let tenphim = req.body.tenphim;
+  let hinhphim = req.body.hinhphim;
+  let trangthai = req.body.trangthai;
+  let thoigian = req.body.thoigian;
+  let idtrailer = req.body.idtrailer;
+  let mota = req.body.mota;
+  let ngaykhoichieu = req.body.ngaykhoichieu;
+  let arrloaiphim = JSON.parse(req.body.arrloaiphim);
+  let sqlquery = `INSERT INTO phim VALUES(NULL,'${tenphim}','${hinhphim}','${trangthai}','${thoigian}','${idtrailer}')`;
+
+  conn.query(sqlquery, function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      console.log(5);
+      let idphim = result.insertId;
+      console.log(idphim);
+      for (let index = 0; index < arrloaiphim.length; index++) {
+        let idloai = arrloaiphim[index].id;
+        let queryphimloai = `INSERT INTO phim_loaiphim VALUES('${idphim}','${idloai}', '${mota}','${ngaykhoichieu}')`;
+        conn.query(queryphimloai, function(err) {
+          if (err) {
+            console.log(err);
+          }
+        });
+      }
+    }
+  });
+});
+
+//
+
+app.get("/loadloaiphimadmin", function(req, res) {
+  let sqlquery = `Select * from loaiphim`;
+  conn.query(sqlquery, function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.post("/deletemovieadmin", function(req, res) {
+  let idphim = req.body.idphim;
+  let sqlquery = `DELETE FROM phim WHERE phim.ID = ${idphim}`;
+  conn.query(sqlquery, function(err, result) {
+    console.log(result);
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+//Customer
+app.get("/loadcustomeradmin", function(req, res) {
+  let sqlquery = `SELECT khachhang.ID, khachhang.HoTen, khachhang.Email, DATE_FORMAT(khachhang.NgaySinh, '%d/%m/%Y') as 'NgaySinh', khachhang.SDT from khachhang`;
+  conn.query(sqlquery, function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.get("/timkiemcustomer", function(req, res) {
+  let tencustomer = req.query.tencustomer;
+  let sqlquery = `SELECT khachhang.ID, khachhang.HoTen, khachhang.Email, DATE_FORMAT(khachhang.NgaySinh, '%d/%m/%Y') as 'NgaySinh', khachhang.SDT FROM khachhang WHERE khachhang.HoTen LIKE '%${tencustomer}%'`;
+  conn.query(sqlquery, function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+//Cinema
+app.get("/loadcinemaadmin", function(req, res) {
+  let strquery = `SELECT * FROM rapphim`;
+  conn.query(strquery, function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+app.post("/updatcinemaadmin", function(req, res) {
+  let idcinema = req.body.idcinema;
+  let tencinema = req.body.tencinema;
+  let hinhcinema = req.body.hinhcinema;
+  let diachicinema = req.body.diachicinema;
+  let sqlquery = `UPDATE rapphim
+  SET rapphim.TenRap = '${tencinema}', rapphim.Hinh = '${hinhcinema}', rapphim.DiaChi = '${diachicinema}'
+  WHERE rapphim.ID = '${idcinema}'`;
+  conn.query(sqlquery, function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send("Cap nhat thanh cong! ");
+    }
+  });
+});
+
+app.post("/insertnewcinemaadmin", function(req, res) {
+  let tencinema = req.body.tencinema;
+  let hinhcinema = req.body.hinhcinema;
+  let diachicinema = req.body.diachicinema;
+  let sqlquery = `INSERT INTO rapphim VALUES(NULL, '${tencinema}', '${hinhcinema}', '${diachicinema}')`;
+  conn.query(sqlquery, function(err, result) {
+    if (err) {
+      res.send(err);
+      console.log(err);
+    } else {
+      res.send("Them thanh cong !");
+    }
+  });
+});
+app.get("/timkiemcinemaadmin", function(req, res) {
+  let tencinema = req.query.tencinema;
+  let sqlquery = `SELECT * FROM rapphim WHERE rapphim.TenRap LIKE '%${tencinema}%'`;
+  conn.query(sqlquery, function(err,result){
+    if(err){
+      res.send(err);
+    }else{
+      res.json(result);
+    }
+  })
+});
+//Ticker
+app.get("/loadtickeradmin", function(req, res) {
+  let sqlquery = `SELECT vedat.ID, vedat.NgayDat, DATE_FORMAT(suatchieu.Gio, '%H:%i') as 'Gio', ghe.TenGhe, phim.TenPhim, khachhang.HoTen, rapphim.TenRap, phong.TenPhong, vedat.Status FROM vedat JOIN suatchieu ON vedat.ID_Suat = suatchieu.ID JOIN ghe ON ghe.ID = vedat.ID_Ghe JOIN phim ON phim.ID = vedat.ID_Phim JOIN khachhang ON khachhang.ID = vedat.ID_KhachHang JOIN rapphim ON rapphim.ID = vedat.ID_Rap JOIN phong ON phong.ID = vedat.ID_Phong`;
+  conn.query(sqlquery, function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+//#endregion
+
+// LIMIT ${vitri},${soluong}
+// LIMIT ${vitribatdau},3
 // GET, POST -> sử dụng query
